fb1096b7e017d5f2b999f2f36da4156c31f9754b
MikkelSvein <109176630+MikkelSvein@users.noreply.github.com>
2025-09-13 08:29:30 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@
 import { NextIntlProvider } from 'next-intl'
 import { useState } from 'react'
 import Navbar from '../components/Navbar'
-import './app/globals.css'
+import './globals.css'
 
 export default function RootLayout({ children }) {
   const [locale, setLocale] = useState('es')
