957d530756df6dd5563e51e3d229a374a1eecf78
MikkelSvein <109176630+MikkelSvein@users.noreply.github.com>
2025-09-13 08:54:28 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 'use client'
 import { useState } from 'react'
 import { IntlProvider } from 'react-intl'
-//import Navbar from '../components/Navbar'
+import Navbar from '../components/Navbar'
 import './globals.css'
 
 const messages = {
